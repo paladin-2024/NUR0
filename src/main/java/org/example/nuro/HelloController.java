a3c10ba454f764b096c6b50ce588f49fa789677e
paladin-2024 <cnzabb@gmail.com>
2025-06-22 23:00:27 +0300

    first commit

---
+++ src/main/java/org/example/nuro/HelloController.java
@@ -0,0 +1,14 @@
+package org.example.nuro;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class HelloController {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
+}

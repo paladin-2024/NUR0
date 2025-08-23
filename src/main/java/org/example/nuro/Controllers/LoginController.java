cff8d929c6ad266502a8feda441b97190ad664e2
paladin-2024 <cnzabb@gmail.com>
2025-08-23 15:32:31 +0300

    feat: add UI components to LoginController for improved user interaction

--- src/main/java/org/example/nuro/Controllers/LoginController.java
+++ src/main/java/org/example/nuro/Controllers/LoginController.java
@@ -1,4 +1,16 @@
 package org.example.nuro.Controllers;
 
+import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
 public class LoginController {
+    public ChoiceBox acc_selector;
+    public Label payee_address_label;
+    public TextField payee_address_field;
+    public Label password_label;
+    public TextField password_field;
+    public Button login_btn;
+    public Label error_label;
 }

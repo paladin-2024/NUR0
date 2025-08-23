1d37956fcf02cd4a8671998919f7bc981376adf5
paladin-2024 <cnzabb@gmail.com>
2025-08-23 15:32:37 +0300

    feat: load FXML for Login scene in App class and set stage properties

--- src/main/java/org/example/nuro/App.java
+++ src/main/java/org/example/nuro/App.java
@@ -1,12 +1,18 @@
 package org.example.nuro;
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class App extends Application {
 
     @Override
     public void start(Stage stage) throws Exception {
-
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/fxml/Login.xml"));
+        Scene scene = new Scene(fxmlLoader.load());
+        stage.setTitle("NURO Banking System");
+        stage.setScene(scene);
+        stage.show();
     }
 }

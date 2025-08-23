4006a711b9d02017a0743c168552d3600fa39bd3
paladin-2024 <cnzabb@gmail.com>
2025-08-23 14:54:08 +0300

    feat: extend App class to inherit from Application and implement start method

--- src/main/java/org/example/nuro/App.java
+++ src/main/java/org/example/nuro/App.java
@@ -1,4 +1,12 @@
 package org.example.nuro;
 
-public class App {
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class App extends Application {
+
+    @Override
+    public void start(Stage stage) throws Exception {
+
+    }
 }
